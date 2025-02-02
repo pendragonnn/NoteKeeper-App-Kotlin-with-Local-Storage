94940086347b3c835c09b158a16735fd8b998da2
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-02 18:40:03 +0700

    feat: [note] add main layout and add note layout screen

--- app/src/main/java/com/example/notekeeper/EditActivity.kt
+++ app/src/main/java/com/example/notekeeper/EditActivity.kt
@@ -1,29 +1,20 @@
 package com.example.notekeeper
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var btnCreate: Button
+class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        btnCreate = findViewById(com.example.notekeeper.R.id.button_create)
-        btnCreate.setOnClickListener {
-            startActivity(Intent(this, EditActivity::class.java))
-        }
-
     }
 }
